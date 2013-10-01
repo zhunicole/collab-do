251da1c5bacfd92e555cf9d519ce323acc25b4ea
Nicole Zhu <zhunickyzhu@gmail.com>
2013-10-01 17:17:00 +0800

    validates email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+
+	validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+  	specify "users' emails should be unique" do
+  		gabe = User.create name: 'Gabe', email: 'gabe@foo.com', password: '123456'
+  		other_gabe = User.new name: 'Gabe', email: 'gabe@foo.com', password: '123456'
+  		other_gabe.save
+		expect(other_gabe.errors_on(:email)).to include("has already been taken")
+  	end
+
+  	specify "users' passwords.length should be > 5" do
+
+  	end
+
+  end
+
+
 end
