1f72380fc6fcc6b95c12354639785c816930f05a
Nicole Zhu <zhunickyzhu@gmail.com>
2013-10-18 11:02:39 +0800

    added carrierwv method to resize to card size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,6 +33,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   process :resize_to_fit => [500, 500]
 
+  version :card_size do
+    process :resize_to_fill => [250, 300]
+  end
+
   version :thumb do
     process :resize_to_fill => [100,100]
   end
