58e747ffdb3188b02f5deac07bfa2c48c118a312
Nicole Zhu <zhunickyzhu@gmail.com>
2013-10-23 16:46:43 +0800

    fixed region problem in fog, photo uploads now work

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAIVSNRJJGGYGNAGTQ',                        # required
     :aws_secret_access_key  => '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',                        # required
-    :region                 => 'Singapore',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :region                 => 'ap-southeast-1',                  # optional, defaults to 'us-east-1'
+    # :host                   => 's3.example.com',             # optional, defaults to nil
+    # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'collabdo'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  # config.fog_public     = false                                   # optional, defaults to true
+  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
